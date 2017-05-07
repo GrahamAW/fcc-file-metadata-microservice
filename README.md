bda49a8b1ea78ca2f23b35e2a576484d0f2a8771
Graham Wood <graham@MacBook-Pro-2.local>
2017-05-07 10:55:28 -0500

    Some general cleanup. Submit button disbaled until a file is selected.

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 This service will allow the user to upload a file using a form. Once the
 file has been upload, the service will return the file size using JSON
-format.
 
 See it in action:
 https://protected-everglades-41584.herokuapp.com/
--- index.js
+++ index.js
@@ -2,13 +2,32 @@
 
 const express = require('express');
 const chalk = require('chalk');
+require('dotenv').config();
+const multer = require('multer');
+const upload = multer({
+  dest: 'uploads/'
+});
 
 const app = express();
-require('dotenv').config();
+app.use(express.static('public'));
+
 const port = process.env.PORT || 80;
 
 app.get('/', (req, res) => {
-  res.end('Hello');
+  res.sendFile('index.html', {
+    root: __dirname + '/public/'
+  });
+});
+
+app.post('/', upload.single('file'), function (req, res) {
+  if (req.file) {
+    var obj = {
+      'size': req.file.size
+    };
+    res.send(obj).end();
+  } else {
+    res.end('No File Uploaded');
+  }
 });
 
 app.listen(port, () => {
